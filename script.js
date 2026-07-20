bf6b9c8a7933e3f3b29b11a8ab4301e9d232152a
UR1616161616 <213202465+UR1616161616@users.noreply.github.com>
2026-07-20 13:09:50 +0900

    作業と休憩の自動切換機能を実装

--- script.js
+++ script.js
@@ -10,12 +10,17 @@
 // console.log("script.js が読み込まれました");
 // ↑ console.log は「開発者ツールのConsoleに文字を出す」命令。動作確認によく使います。
 const startButton = document.querySelector(".btn-start");
-const timeDisplay = document.querySelector(".display")
+const stopButton = document.querySelector(".btn-stop");
+const resetButton = document.querySelector(".btn-reset");
+const timeDisplay = document.querySelector(".display");
+const statusText = document.querySelector(".status");
 
 let timeLeft = 25 * 60;
+let timerId = null;
+let isWorking = true;
 
 // 残り秒数を「分:秒」にして画面に表示する“関数
-function updateDisplay(){
+function updateDisplay() {
     const minutes = Math.floor(timeLeft / 60);
     const seconds = timeLeft % 60;
 
@@ -25,9 +30,47 @@ function updateDisplay(){
     timeDisplay.textContent = m + ":" + s;
 }
 
-startButton.addEventListener("click", function(){
-    setInterval(function(){
+function switchMode(){
+    if(isWorking == true){
+        isWorking = false;
+        timeLeft = 5 * 60;
+        statusText.textContent = "休憩中";
+        document.body.style.backgroundColor = "#4f9d69";
+    }else{
+        isWorking = true;
+        timeLeft = 25 * 60;
+        statusText.textContent = "作業中"
+        document.body.style.backgroundColor = "#d94f4f";
+    }
+        
+}
+
+startButton.addEventListener("click", function () {
+
+    if (timerId !== null) {
+        return;
+    }
+    timerId = setInterval(function () {
         timeLeft = timeLeft - 1;
+        if(timeLeft < 0){
+            switchMode();
+        }
+
         updateDisplay();
     }, 1000);
+});
+
+stopButton.addEventListener("click", function() {
+    clearInterval(timerId);
+    timerId = null;
+});
+
+resetButton.addEventListener("click", function() {
+    clearInterval(timerId);
+    timerId = null;
+    isWorking = true;
+    timeLeft = 25 * 60;
+    statusText.textContent = "作業中";
+    document.body.style.backgroundColor = "#d94f4f";
+    updateDisplay();
 });
